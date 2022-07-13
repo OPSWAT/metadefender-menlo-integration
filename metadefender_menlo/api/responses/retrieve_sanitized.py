3689832b6d3c87b00500842151beeb67208c9752
Dan Ciobanu <dan.ciobanu@opswat.com>
2022-07-13 12:09:51 +0300

    send 204 when sanitization file is not available

--- metadefender_menlo/api/responses/retrieve_sanitized.py
+++ metadefender_menlo/api/responses/retrieve_sanitized.py
@@ -6,12 +6,13 @@ class RetrieveSanitized(BaseResponse):
 
     def __init__(self, allowedResponses=None):
 
-        allowedResponses = [200, 404, 405,403, 500]
+        allowedResponses = [200, 404,204, 405,403, 500]
         super().__init__(allowedResponses)
 
         self._http_responses["403"] = self.__response403
         self._http_responses["404"] = self.__response404
         self._http_responses["405"] = self.__response405
+        self._http_responses["204"] = self.__response204
 
     def __response404(self, response, status_code):
         return (response, 400)
@@ -21,5 +22,8 @@ class RetrieveSanitized(BaseResponse):
         
     def __response403(self, response, status_code):
         return (response, 403)
+    def __response204(self, response, status_code):
+        return (response, 204)
+   
     
     
