7d419171222b3d1cbd0a89c3fc7a7efdc8a58d43
Razvan Tamsa <razvan.tamsa@opswat.com>
2022-08-26 11:39:37 +0300

    feature/MDCL-5993 return 200 with json on 404 sha256

--- metadefender_menlo/api/responses/check_existing.py
+++ metadefender_menlo/api/responses/check_existing.py
@@ -6,10 +6,11 @@ class CheckExisting(BaseResponse):
     
     def __init__(self, allowedResponses=None):
         
-        allowedResponses = [200, 500]
+        allowedResponses = [200, 400, 500]
         super().__init__(allowedResponses)
 
         self._http_responses["200"] = self.__response200
+        self._http_responses["400"] = self.__response400
 
     def __response200(self, response, status_code):
         translation = {
@@ -26,3 +27,8 @@ class CheckExisting(BaseResponse):
         else:
             return (response, 404)
 
+    def __response400(self, response, status_code):
+            return ({
+                'uuid': response['sha256'],
+                'result': '404'
+            }, 200)
