aaeb4bcf34d3af2afd68fde11018e311a0d6177d
Andrei Domsa <andrei.domsa@opswat.com>
2022-09-23 16:14:40 +0300

    bump version 1.3.2

--- metadefender_menlo/api/handlers/health_check.py
+++ metadefender_menlo/api/handlers/health_check.py
@@ -13,7 +13,7 @@ class HealthCheckHandler(BaseHandler):
         self.write(json.dumps({
             "status": "Ready",
             "name": "MetaDefender - Menlo integration",
-            "version": "1.3.1",
+            "version": "1.3.2",
             "commitHash": environ.get("BITBUCKET_COMMIT_HASH", "-"),
             "rule": environ.get("MDCLOUD_RULE","multiscan, sanitize, unarchive")
         }))
