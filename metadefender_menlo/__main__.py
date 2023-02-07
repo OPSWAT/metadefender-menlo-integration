827f33a3d2a1ebcac115d950fe960ce3bab2843f
Dan Ciobanu <dan.ciobanu@opswat.com>
2023-02-07 14:03:14 +0200

    set MDCLOUD_URL at deploy

--- metadefender_menlo/__main__.py
+++ metadefender_menlo/__main__.py
@@ -143,7 +143,11 @@ def initial_config(config_path, sns_config_path):
 
     api = config["api"]
     md_type = api["type"]
-    url = api["url"][md_type] if "url" in api and md_type in api["url"] else "http://localhost:8008"
+    
+    url = environ.get("MDCLOUD_URL", "https://api.metadefender.com/v4")
+    if md_type == "core":
+        url = api["url"][md_type] if "url" in api and  "core" in api["url"] else "http://localhost:8008"
+
     apikey = api["params"]["apikey"] if "params" in api and "apikey" in api["params"] else None
 
     env_apikey = os.environ.get('apikey')
