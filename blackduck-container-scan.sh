8dd157a505ab9d487e2d87c55a1df72cfa976854
Raul Raveica <raul.raveica@opswat.com>
2023-03-22 17:27:16 +0200

    MDCL-6536 - Blackduck container scanning

---
+++ blackduck-container-scan.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+while [[ $# -gt 0 ]]; do
+  case "${1}" in
+    -t|--token)
+      shift
+      BLACKDUCK_TOKEN=${1}
+      shift
+      ;;
+    -i|--image)
+      shift
+      IMAGE=${1}
+      shift
+      ;;
+     *)
+      shift
+      echo "Unsupported parameter"
+      echo "Supported parameters are: -t --token -i --image"
+      exit 1
+      ;;
+  esac
+done
+
+MENLO_VERSION=$(awk '/VERSION = / {print $3}' setup.py)
+
+bash <(curl -s -L https://detect.synopsys.com/detect8.sh) \
+	--blackduck.url=https://opswat.blackducksoftware.com  \
+	--blackduck.api.token=\"${BLACKDUCK_TOKEN}\" \
+	--blackduck.trust.cert=true \
+	--detect.docker.image="${IMAGE}" \
+	--detect.project.name=\"MD Cloud Menlo\" \
+	--detect.project.version.name=menlo-plugin-$MENLO_VERSION-container \
+	--detect.project.version.phase=DEVELOPMENT \
+	--detect.tools.excluded=BINARY_SCAN \
+	--logging.level.com.synopsys.integration=DEBUG
