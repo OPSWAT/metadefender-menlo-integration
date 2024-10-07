80e7250f6e3c79dd35f7e75103cd3fc206cde398
Andrei Sferdian <andrei.sferdian@opswat.com>
2024-10-07 14:57:23 +0300

    Add cli

---
+++ menlo.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+HELP_MENLO="
+    setup                   # setup environment
+    help                    # show this message
+    unit                    # run unit tests
+    integration             # run integration tests
+"
+
+function printHelp {
+    echo -e "${HELP_MENLO}"
+}
+
+function setup {
+    echo "Setting up environment..."
+    
+    which python3 &> /dev/null || (echo "Python3 is not installed" && exit 1)
+    python3 --version
+    
+    if [ ! -d .venv ]; then
+        echo "Creating virtual environment..." 
+        python3 -m venv .venv
+    fi
+
+    source .venv/bin/activate
+
+    echo "Installing dependencies..."
+    pip install -r requirements.txt
+}
+
+if [[ $# == 0 ]]; then
+    printHelp
+    exit 0
+fi
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -h|--help|help)
+            printHelp
+            exit 0
+            ;;
+        setup)
+            setup
+            ;;
+        unit)
+            echo "Running unit tests..."
+            python -m unittest discover -v ./tests/unit -p 'test*.py'
+            ;;
+        integration)
+            echo "Running integration tests..."
+            python -m unittest discover -v ./tests/integration -p 'tests*.py'
+            ;;
+        *)
+            echo "Unknown parameter: ${1}"
+            printHelp
+            exit 1
+    esac
+
+    shift
+done 
--- tc-ci/sonarqube-scan.sh
+++ tc-ci/sonarqube-scan.sh
@@ -19,27 +19,6 @@ if [ $? != "0" ]; then
 	echo "10.192.9.121 sonar.opswat.com" >> /etc/hosts
 fi
 
-# TODO: Create a docker image with python 3.12
-# install python 3.12
-apt-get update && apt-get install -y \
-    make build-essential libssl-dev zlib1g-dev \
-    libbz2-dev libreadline-dev libsqlite3-dev wget curl \
-    llvm libncurses5-dev libncursesw5-dev \
-    xz-utils tk-dev libffi-dev liblzma-dev python-openssl \
-    git
-
-curl https://pyenv.run | bash
-
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
-pyenv install 3.12
-
-# generate coverage report
-make init && make coverage 
-
 docker run \
     --rm \
     --network=host \
