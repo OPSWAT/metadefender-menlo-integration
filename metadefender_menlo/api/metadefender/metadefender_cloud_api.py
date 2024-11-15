d6c4ef40dd2feda5eb3ad382775e669efc8703d4
Sebastian Stoicescu <sebastian.stoicescu@opswat.com>
2024-11-15 17:50:13 +0200

    add encoder for special characters

--- metadefender_menlo/api/metadefender/metadefender_cloud_api.py
+++ metadefender_menlo/api/metadefender/metadefender_cloud_api.py
@@ -28,7 +28,7 @@ class MetaDefenderCloudAPI(MetaDefenderAPI):
         else:
             file_name = None 
         headers = {
-            "filename": file_name or urllib.parse.quote(filename),
+            "filename": urllib.parse.quote(file_name) or urllib.parse.quote(filename),
             "Content-Type": "application/octet-stream",
             "rule": self.settings['scanRule']
         }
