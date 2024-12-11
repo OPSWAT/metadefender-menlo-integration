69cf5023fd94fa8b6b18e429ab9ec23b34198ba5
Sebastian Stoicescu <sebastian.stoicescu@opswat.com>
2024-12-11 14:28:52 +0200

    Revert "bump version"
    This reverts commit 5109e9ab07b1c28dec11298eb46506e840d4c881.

--- tests/unit/test_handlers_health_check.py
+++ tests/unit/test_handlers_health_check.py
@@ -58,7 +58,7 @@ class TestHealthCheckHandler(unittest.TestCase):
         expected_response = {
             "status": "Ready",
             "name": "MetaDefender - Menlo integration",
-            "version": "1.6.2",
+            "version": "1.6.1",
             "commitHash": self.test_config['commitHash'],
             "rule": self.test_config['scanRule']
         }
@@ -103,7 +103,7 @@ class TestHealthCheckHandler(unittest.TestCase):
 
         self.assertEqual(response_dict['status'], 'Ready')
         self.assertEqual(response_dict['name'], 'MetaDefender - Menlo integration')
-        self.assertEqual(response_dict['version'], '1.6.2')
+        self.assertEqual(response_dict['version'], '1.6.1')
         self.assertEqual(response_dict['commitHash'], self.test_config['commitHash'])
         self.assertEqual(response_dict['rule'], self.test_config['scanRule'])
 
