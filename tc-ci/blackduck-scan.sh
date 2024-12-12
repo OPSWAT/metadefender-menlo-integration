472ddb729edb153aefef31074390fd067bbfa235
Andrei Brindas <andrei.brindas@opswat.com>
2024-12-12 15:36:08 +0200

    MDCL-9578 menlo bd scan branch naming

--- tc-ci/blackduck-scan.sh
+++ tc-ci/blackduck-scan.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Usage:
-# 
-# export BD_TOKEN=?
-# export BD_VERSION_PHASE=?
-#
-# ./tc-ci/blackduck-scan.sh
-
 CWD=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd );
 cd $CWD/..
-
-PLUGIN_VERSION=$(awk '/VERSION = / {print $3}' setup.py)
+BRANCH="$(git branch --show-current)"
 
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
+
+if [[ "$BRANCH" == "master" || "$BRANCH" == "main" ]]; then
+    DETECT_PROJECT_VERSION_NAME="Community-main-container"
+elif [[ "$BRANCH" == "release" ]]; then
+    DETECT_PROJECT_VERSION_NAME="Community-Release-HEAD-container"
+else
+    DETECT_PROJECT_VERSION_NAME="Community-${COMMUNITY_VERSION}-container"
+fi
+
 
 bash <(curl -s -L https://detect.synopsys.com/detect9.sh) --detect.timeout=3600\
     --blackduck.api.token=\"${BD_TOKEN}\" \
@@ -23,9 +24,9 @@ bash <(curl -s -L https://detect.synopsys.com/detect9.sh) --detect.timeout=3600\
     --detect.notices.report=true \
     --detect.pip.requirements.path=requirements.txt \
     --detect.policy.check.fail.on.severities=BLOCKER,CRITICAL \
-    --detect.project.name=\"MD Cloud Menlo\" \
-    --detect.project.version.name=menlo-plugin-$PLUGIN_VERSION \
-    --detect.project.version.phase=\"${BD_VERSION_PHASE}\" \
+    --detect.project.name="MD Cloud Menlo" \
+    --detect.project.version.name="${DETECT_PROJECT_VERSION_NAME}" \
+    --detect.project.version.phase="${BD_VERSION_PHASE}" \
     --detect.python.path=/usr/bin/python3 \
     --detect.tools.excluded=SIGNATURE_SCAN \
     --logging.level.com.synopsys.integration=DEBUG
