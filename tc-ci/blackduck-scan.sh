3bd276a352672ee16bd79cb262b6691bc88a8757
Andrei Brindas <andrei.brindas@opswat.com>
2024-12-18 16:11:42 +0200

    update scanner service

--- tc-ci/blackduck-scan.sh
+++ tc-ci/blackduck-scan.sh
@@ -4,17 +4,33 @@ CWD=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd );
 cd $CWD/..
 BRANCH="$(git branch --show-current)"
 PLUGIN_VERSION=$(awk '/VERSION = / {print $3}' setup.py)
-
+BD_PARENT_PROJECT="MD Cloud Menlo"
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 echo "${BRANCH}"
-if [[ "$BRANCH" == "master" || "$BRANCH" == "main" ]]; then
-    DETECT_PROJECT_VERSION_NAME="main"
-elif [[ "$BRANCH" == "release" ]]; then
-    DETECT_PROJECT_VERSION_NAME="Release-HEAD"
-else
-    DETECT_PROJECT_VERSION_NAME="MD Cloud Menlo-${PLUGIN_VERSION}"
-fi
+
+case $BRANCH in
+    customer)
+        # customer branch / tag
+        BD_PROJECT_VERSION=$BD_PARENT_PROJECT-$VERSION
+        BD_VERSION_PHASE="RELEASED"
+    ;;
+    release*)
+        # release branch
+        BD_PROJECT_VERSION=$BD_PARENT_PROJECT-Release-HEAD
+        BD_VERSION_PHASE="PRERELEASE"
+    ;;
+    master)
+        # master branch
+        BD_PROJECT_VERSION=$BD_PARENT_PROJECT-Develop-HEAD
+        BD_VERSION_PHASE="DEVELOPMENT"
+    ;;
+    *)
+        #other branches
+        BD_PROJECT_VERSION=$BD_PARENT_PROJECT-$BRANCH_NAME
+        BD_VERSION_PHASE="DEVELOPMENT"
+    ;;
+esac;
 echo "${DETECT_PROJECT_VERSION_NAME}"
 
 
@@ -26,8 +42,8 @@ bash <(curl -s -L https://detect.synopsys.com/detect9.sh) --detect.timeout=3600\
     --detect.notices.report=true \
     --detect.pip.requirements.path=requirements.txt \
     --detect.policy.check.fail.on.severities=BLOCKER,CRITICAL \
-    --detect.project.name="MD Cloud Menlo" \
-    --detect.project.version.name="${DETECT_PROJECT_VERSION_NAME}" \
+    --detect.project.name="${BD_PARENT_PROJECT}" \
+    --detect.project.version.name="${BD_PROJECT_VERSION}" \
     --detect.project.version.phase="${BD_VERSION_PHASE}" \
     --detect.python.path=/usr/bin/python3 \
     --detect.tools.excluded=SIGNATURE_SCAN \
