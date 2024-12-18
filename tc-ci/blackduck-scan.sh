8a81757225668c4a8592acdfecd2b50f337e79a9
Andrei Brindas <andrei.brindas@opswat.com>
2024-12-18 17:17:39 +0200

    update scanner service

--- tc-ci/blackduck-scan.sh
+++ tc-ci/blackduck-scan.sh
@@ -5,38 +5,44 @@ cd $CWD/..
 BRANCH="$(git branch --show-current)"
 PLUGIN_VERSION=$(awk '/VERSION = / {print $3}' setup.py)
 BD_PARENT_PROJECT="MD Cloud Menlo"
+VERSION=${PLUGIN_VERSION:-"unknown"}  # Use PLUGIN_VERSION or fallback to "unknown"
+BRANCH_NAME=${BRANCH}  # Ensure BRANCH_NAME is initialized
+
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
+
 echo "BRANCH: ${BRANCH}"
 BD_PROJECT_VERSION=""
 BD_VERSION_PHASE=""
+
 case $BRANCH in
     customer)
         # customer branch / tag
-        BD_PROJECT_VERSION=$BD_PARENT_PROJECT-$VERSION
+        BD_PROJECT_VERSION="${BD_PARENT_PROJECT}-${VERSION}"
         BD_VERSION_PHASE="RELEASED"
     ;;
     release*)
         # release branch
-        BD_PROJECT_VERSION=$BD_PARENT_PROJECT-Release-HEAD
+        BD_PROJECT_VERSION="${BD_PARENT_PROJECT}-Release-HEAD"
         BD_VERSION_PHASE="PRERELEASE"
     ;;
     master)
         # master branch
-        BD_PROJECT_VERSION=$BD_PARENT_PROJECT-Develop-HEAD
+        BD_PROJECT_VERSION="${BD_PARENT_PROJECT}-Develop-HEAD"
         BD_VERSION_PHASE="DEVELOPMENT"
     ;;
     *)
-        #other branches
-        BD_PROJECT_VERSION=$BD_PARENT_PROJECT-$BRANCH_NAME
+        # other branches
+        BD_PROJECT_VERSION="${BD_PARENT_PROJECT}-${BRANCH_NAME}"
         BD_VERSION_PHASE="DEVELOPMENT"
     ;;
-esac;
+esac
+
 echo "DETECT_PROJECT_VERSION_NAME: ${BD_PROJECT_VERSION}"
 
 
-bash <(curl -s -L https://detect.synopsys.com/detect9.sh) --detect.timeout=3600\
-    --blackduck.api.token=\"${BD_TOKEN}\" \
+bash <(curl -s -L https://detect.synopsys.com/detect9.sh) --detect.timeout=3600 \
+    --blackduck.api.token="${BD_TOKEN}" \
     --blackduck.url=https://opswat.blackducksoftware.com  \
     --detect.blackduck.signature.scanner.upload.source.mode=false \
     --detect.detector.search.exclusion.paths=docker \
