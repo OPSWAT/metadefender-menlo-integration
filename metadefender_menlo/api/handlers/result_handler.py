dae2903b0f12c3adcb0688654d79927c1c2a53cf
Catalin Manasia <catalin.manasia@opswat.com>
2025-09-30 17:51:34 +0300

    MDCI-55 Added timeout for result response

--- metadefender_menlo/api/handlers/result_handler.py
+++ metadefender_menlo/api/handlers/result_handler.py
@@ -1,13 +1,25 @@
 import logging
+import asyncio
+import yaml
 from fastapi import Request, Response
 from metadefender_menlo.api.responses.file_analysis import FileAnalyis
 from metadefender_menlo.api.handlers.base_handler import BaseHandler
 from metadefender_menlo.api.log_types import SERVICE, TYPE
 
+with open('config.yml', 'r') as file:
+    config = yaml.safe_load(file)
+
+if config['timeout']['result']['enabled']:
+    timeout = config['timeout']['result']['value']
+else:
+    timeout = None 
+
 class ResultHandler(BaseHandler):
     """
     Handler for retrieving the result of a file analysis using its UUID.
     """
+    timeout = timeout
+    print('### timeout: ', timeout)
     def __init__(self):
         super().__init__()
 
@@ -23,12 +35,34 @@ class ResultHandler(BaseHandler):
             {"method": "GET", "endpoint": "/api/v1/result/%s" % uuid}
         ))
         
-        await self.prepare_request(request)
+        await self.prepare_request(request) # TODO: also do the timeout here for request (to MDCL)
 
         try:
-            json_response, http_status = await self.meta_defender_api.check_result(uuid, self.apikey, self.client_ip)
+            timeout_value = None
+            try:
+                if self.timeout is not None:
+                    timeout_value = float(self.timeout)
+            except Exception:
+                timeout_value = None
+
+            if timeout_value is not None:
+                json_response, http_status = await asyncio.wait_for(
+                    self.meta_defender_api.check_result(uuid, self.apikey, self.client_ip),
+                    timeout=timeout_value
+                )
+            else:
+                json_response, http_status = await self.meta_defender_api.check_result(uuid, self.apikey, self.client_ip)
+
             json_response, http_status = await FileAnalyis(self.apikey).handle_response(json_response, http_status)
             return self.json_response(response, json_response, http_status)
+        except asyncio.TimeoutError:
+            logging.error("{0} > {1} > {2}".format(
+                self.meta_defender_api.service_name, 
+                TYPE.Response, 
+                {"error": f"Timeout while retrieving result for {uuid}"}
+            ))
+            print('### timeouting...')
+            return self.json_response(response, {}, 500)
         except Exception as error:
             logging.error("{0} > {1} > {2}".format(
                 self.meta_defender_api.service_name, 
