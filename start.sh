753d33b0c007f7f85aeb61f993ea991fd890263d
Paula Ciocu <paulaciocu@opswat.com>
2025-11-06 16:29:02 +0200

    MDCL-13828 Fix script

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# Define the log file for the ClamAV scan report (use /tmp to avoid permission issues)
-SCAN_REPORT="/tmp/clamav_scan_report.txt"
+# Define the log file for the ClamAV scan report
+SCAN_REPORT="/var/log/clamav_scan_report.txt"
 
-# Remove old log file if it exists to avoid locking issues
+mkdir -p /var/log
 rm -f "$SCAN_REPORT"
 
 # Perform a ClamAV scan on the entire container's filesystem
 echo "Starting ClamAV scan..."
-clamscan -r --bell -i / --exclude-dir="^/proc" --exclude-dir="^/sys" --exclude-dir="^/dev" --exclude-dir="^/tmp" --log="$SCAN_REPORT"
+clamscan -r --bell -i / --exclude-dir="^/proc" --exclude-dir="^/sys" --exclude-dir="^/dev" --exclude-dir="^/var/log" --log=$SCAN_REPORT
 
 # Check the scan result and print it
 echo "ClamAV scan completed. Report saved to $SCAN_REPORT."
